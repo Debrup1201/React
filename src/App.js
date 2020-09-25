8f6f401f14062b9333f5edbb20cf1f85207c6c89
Debrup Mandal <mandaldebrup1201@gmail.com>
2020-09-25 20:00:25 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import Menu from './components/MenuComponent';
+// import './App.css';
 
 class App extends Component {
   render() {
@@ -14,6 +14,7 @@ class App extends Component {
             </NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
